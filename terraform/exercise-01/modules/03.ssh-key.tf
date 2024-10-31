2204ca0a51a993436e4c81ed8cbed22e43ab97d6
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2024-10-31 13:31:55 -0300

    Delete terraform directory
    ad

--- terraform/exercise-01/modules/03.ssh-key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "nginx-server-ssh-key" {  
-  key_name   = "${var.server_name}-ssh-key"
-  public_key = file("${var.server_name}.pub")
-}
--- terraform/exercise-01/s3-backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "juan-tf-backend-dev-prod"
-    key            = "./terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
