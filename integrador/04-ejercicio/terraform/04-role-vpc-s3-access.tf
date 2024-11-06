03b3ab1cee92a009c4ee1aa7b7e4c8db5361efb0
Full-Juan-Ortega <juan.ortega.it@gmail.com>
2024-11-06 20:34:21 -0300

    avances sobre el ejercicio 4

---
+++ integrador/04-ejercicio/terraform/04-role-vpc-s3-access.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_role" "vpc_s3_access_role" {
+  name = "VPC-S3-AccessRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "vpc_s3_policy" {
+  name        = "VPC-S3-AccessPolicy"
+  description = "Policy to access VPC and S3 bucket node-app-backup"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::node-app-backup",
+          "arn:aws:s3:::node-app-backup/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_s3_policy_attachment" {
+  role       = aws_iam_role.vpc_s3_access_role.name
+  policy_arn = aws_iam_policy.vpc_s3_policy.arn
+}
